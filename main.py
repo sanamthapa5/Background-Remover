7b1752f42baad1a27110f8b4828a8fc8056decb3
Sanam <Sanamthapa112233@gmail.com>
2025-04-10 20:22:07 +0545

    Image Background Remover

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from rembg import remove
 import easygui
-from PIL import Image
 
-inputPath = easygui.fileopenbox(title='Select image')
-outputPath = easygui.filesavebox(title='Preferred location to save image', default='*.png')
+from PIL import Image  
+inputPath=easygui.fileopenbox(title='select image')
+outputPath=easygui.filesavebox(title='Prefered location to save image')
 
-if inputPath and outputPath:
-    try:
-        input_image = Image.open(inputPath)
-        output = remove(input_image)
-        output.save(outputPath)
-        print("Image saved successfully.")
-    except Exception as e:
-        print("Error:", e)
-else:
-    print("Operation cancelled by user.")
+input=Image.open(inputPath)
+output=remove(input)
+output.save(outputPath)
+
+      
